fa72cbc1762737d23ec8cfe7421eb090a6fd20ef
Michael Bekker <bekker.michael2@gmail.com>
2020-04-01 19:20:56 +0200

    Added paperclip gem

--- db/migrate/20200331191752_create_taggings.rb
+++ db/migrate/20200331191752_create_taggings.rb
@@ -1,8 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration[6.0]
   def change
     create_table :taggings do |t|
-      t.references :tag, null: false, foreign_key: false
-      t.references :article, null: false, foreign_key: false
+      t.references :tag, null: false, foreign_key: true, 
+      t.references :article, null: false, foreign_key: true,
 
       t.timestamps
     end
